87a8f27deb9d68e2acfa8cd823267aea59b99017
Mona Bocharova <bocharova.mona@gmail.com>
2018-10-16 16:19:07 -0700

    client side post request and server side post request both work

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,28 @@
+$(document).ready(function () {
 
+    $('#submit').click(function(e) {
+        e.preventDefault();
+        var input = $('#text').val();
+        serverRequests.create({firstname: input});
+    });
+
+});
+
+var serverRequests = {
+
+    create: function(obj) {
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost:3000',
+            data: JSON.stringify(obj),
+            contentType:"application/json",
+            success: (data) => {
+                console.log(data);
+            },
+            error: (err)=> {
+               console.log(err);
+           }
+        })
+    }
+};
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,46 @@
+var cors = require('cors');
+var express = require('express');
+var bodyparser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+var port = 3000;
 
+var app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+app.post('/', (req, res) => {
+    // var str = '';
+    // req.setEncoding('utf8');
+    //
+    // req.on('data', function(chunk) {
+    //     str += chunk;
+    // });
+    //
+    // req.on('end', function() {
+    //     console.log(str);
+    //     res.end(json.stringify(str));
+    // });
+    //
+    //
+    var data = req.body;
+    // var fields = Object.keys(data);
+    // var replacer = function(key, value) { return value === null ? '' : value };
+    // var csv = fields.map(function(row){
+    //     return fields.map(function(fieldName){
+    //         return JSON.stringify(row[fieldName], replacer)
+    //     }).join(',')
+    // });
+    // csv.unshift(fields.join(',')); // add header column
+    //
+    // var finalcsv = csv.join('\r\n');
+
+    res.writeHead(201, { 'Content-Type': 'application/octet-stream' });
+    res.end(JSON.stringify(data));
+});
 
