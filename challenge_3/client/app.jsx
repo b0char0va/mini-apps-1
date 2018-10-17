1c25c2cfd4fb9f5ccb153c9b5d4eb9ed9cfe7f8e
Mona Bocharova <bocharova.mona@gmail.com>
2018-10-17 14:51:28 -0700

    forms working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,113 @@
+var Checkout = (props) => (
+    <button id="checkout" onClick = {props.clickHandler}>Checkout!!</button>
+);
+
+
+var Form1 = (props) => (
+    <form className="forms">
+        <label>
+            Name
+    <input type="text"/>
+        </label><br/>
+        <label>
+            Email
+    <input type="text"/>
+        </label><br/>
+        <label>
+            Password
+    <input type="text"/>
+        </label>
+        <input type="submit" value="Submit" onClick = {props.clickHandler}/>
+    </form>
+);
+
+var Form2 = (props) => (
+    <form className="forms">
+        <label>
+            Address
+            <input type="text"/>
+        </label><br/>
+        <label>
+            City
+            <input type="text"/>
+        </label><br/>
+        <label>
+            State
+            <input type="text"/>
+        </label>
+        <label>
+            ZipCode
+            <input type="text"/>
+        </label>
+        <input type="submit" value="Submit" onClick = {props.clickHandler}/>
+    </form>
+);
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            viewForm1 : false,
+            viewForm2 : false,
+            viewCheckoutButton : true
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleFinalSubmit = this.handleFinalSubmit.bind(this);
+        this.form = <Checkout clickHandler = {this.handleClick}/>;
+    }
+
+    handleSubmit (e) {
+        e.preventDefault();
+        console.log('hi');
+        switch(this.state.viewForm2) {
+            case false:
+                this.form = <Form2 clickHandler = {this.handleFinalSubmit}/>;
+                break;
+            default:
+                this.form = <Checkout clickHandler = {this.handleClick}/>;
+        }
+        this.setState({
+            viewForm2 : true,
+            viewForm1 : false,
+            viewCheckoutButton : false
+        })
+    }
+
+    handleFinalSubmit (e) {
+        e.preventDefault();
+        console.log('hey');
+    }
+
+    handleClick (){
+        switch(this.state.viewForm1) {
+            case false:
+                this.form = <Form1 clickHandler = {this.handleSubmit}/>;
+                break;
+            default:
+                this.form = <Checkout clickHandler = {this.handleClick}/>;
+        }
+        this.setState({
+            viewForm1 : true,
+            viewCheckoutButton : false
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                {(this.state.viewForm1) ?
+                    this.form : ''}
+                {(this.state.viewForm2) ?
+                    this.form : ''}
+                {(this.state.viewCheckoutButton) ?
+                    this.form : ''}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`);
+});
+
+
+
+
+
 
 
