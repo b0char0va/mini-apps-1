f2b55aef832dfbd5ac043948bc56fd0bc481ed45
Mona Bocharova <bocharova.mona@gmail.com>
2018-10-16 20:02:37 -0700

    trying to refactor code to use file picker instead of text area

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
 
-    $('#submit').click(function(e) {
-        e.preventDefault();
-        var input = $('#text').val();
-        var data = JSON.parse(input);
-        serverRequests.create(data, modifyHTML.show);
+    // $('#submit').click(function(e) {
+    //     e.preventDefault();
+    //     var input = $('#text').val();
+    //     var data = JSON.parse(input);
+    //     serverRequests.create(data, modifyHTML.show);
+    // });
+
+    // $("#file").change(function() {
+    //     serverRequests.create(data, modifyHTML.show);
+    // });
+
+
+    $("#submit").click(function(evt) {
+        evt.preventDefault();
+        var form = $('#fileinfo');
+        var formData = new FormData(form[0]);
+        // console.log("hi");
+        // console.log(formData);
+        serverRequests.create(formData);
     });
 
 });
@@ -16,18 +30,37 @@ var modifyHTML = {
 };
 
 
+// var serverRequests = {
+//
+//     create: function(obj, successCB) {
+//         $.ajax({
+//             type: "POST",
+//             url: 'http://localhost:3000',
+//             data: JSON.stringify(obj),
+//             contentType:"application/json",
+//             success: successCB,
+//             error: (err)=> {
+//                console.log(err);
+//            }
+//         })
+//     }
+// };
+
+
 var serverRequests = {
 
-    create: function(obj, successCB) {
+    create: function(obj) {
         $.ajax({
-            type: "POST",
-            url: 'http://localhost:3000',
-            data: JSON.stringify(obj),
-            contentType:"application/json",
-            success: successCB,
-            error: (err)=> {
-               console.log(err);
-           }
+            type: 'POST',
+            data: obj,
+            async: false,
+            cache: false,
+            contentType: false,
+            enctype: 'multipart/form-data',
+            processData: false,
+            success: function (response) {
+                console.log(response);
+            }
         })
     }
 };
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,34 +3,46 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
+var busboy = require('connect-busboy');
 var port = 3000;
 
 var app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
+app.use(busboy());
 app.use(express.static(path.join(__dirname, '/client')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 app.post('/', (req, res) => {
-    var data = [req.body];
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+        res.writeHead(201, {'Content-Type': 'text/plain'});
+        res.end("hey");
+    })
 
-    var flattened = start(data);
-    var filteredArray = removeChildrenProp(flattened);
 
-    var fields = Object.keys(filteredArray[0]);
-    var csv = filteredArray.map(function (row) {
-        return fields.map(function (fieldName) {
-            return (row[fieldName]);
-        }).join(',')
-    });
-    csv.unshift(fields.join(',')); // add header column
-    var finalcsv = csv.join('\r\n');
 
-    res.writeHead(201, {'Content-Type': 'application/octet-stream'});
-    res.end(finalcsv);
+
+    // var data = [req.body];
+    //
+    // var flattened = start(data);
+    // var filteredArray = removeChildrenProp(flattened);
+    //
+    // var fields = Object.keys(filteredArray[0]);
+    // var replacer = function(key, value) { return value === null ? '' : value };
+    // var csv = filteredArray.map(function (row) {
+    //     return fields.map(function (fieldName) {
+    //         return JSON.stringify(row[fieldName], replacer)
+    //     }).join(',')
+    // });
+    // csv.unshift(fields.join(',')); // add header column
+    // var finalcsv = csv.join('\r\n');
+    // console.log(finalcsv);
+    // res.writeHead(201, {'Content-Type': 'application/octet-stream'});
+    // res.end(finalcsv);
 });
 
 
@@ -38,7 +50,6 @@ app.post('/', (req, res) => {
 var start = function(arr) {
     var data = JSON.parse(JSON.stringify(arr));
     var array = [data[0]];
-
 
     if (data[0].children && data[0].children.length > 0) {
         var recurse = function (arr) {
@@ -59,7 +70,9 @@ var start = function(arr) {
 var removeChildrenProp = function (arr) {
     var newArr = [];
     for(var i = 0; i < arr.length; i++){
-       delete arr[i]['children'];
+        if(arr[i]['children']){
+            delete arr[i]['children'];
+        }
        newArr.push(arr[i]);
     }
     return newArr;
